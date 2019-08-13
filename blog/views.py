b707532a62fcbbb8a6612fb37289abe0d91e3493
vo0doo <kirsanov.bvt@gmail.com>
2019-08-13 20:17:12 +0300

    init

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+
+def post_list(request):
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
