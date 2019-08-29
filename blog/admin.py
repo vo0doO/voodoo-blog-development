35aa3a9bed0dc309106480caf22cc3483de59690
vo0doo <kirsanov.bvt@gmail.com>
2019-08-29 20:19:00 +0300

    рабочая версия с моножеством вопросов с множественным выбором

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Client, Answer, Choice, Question
+from .models import Client, Answer
 
 admin.site.register(Client)
 admin.site.register(Answer)
-admin.site.register(Choice)
-admin.site.register(Question)
