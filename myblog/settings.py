872d0eba232d0b122f51bb5610c7783531d32b4e
vo0doo <kirsanov.bvt@gmail.com>
2019-10-02 19:02:46 +0300

    add-more-and-add-more-and

--- myblog/settings.py
+++ myblog/settings.py
@@ -143,7 +143,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
    os.path.join(BASE_DIR, 'static'),
    os.path.join(BASE_DIR, 'media'),
-   os.path.join(BASE_DIR, 'static', 'media'),
 )
 
 # # Медиа
